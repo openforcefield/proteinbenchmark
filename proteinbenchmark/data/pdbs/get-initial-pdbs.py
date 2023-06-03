9fd3026452b13e4c9ea6b5cb59f9b342758ed4ef
Chapin Cavender <chapincavender@gmail.com>
2023-06-02 18:10:07 -0700

    Targets for BPTI and Ubiquitin

--- proteinbenchmark/data/pdbs/get-initial-pdbs.py
+++ proteinbenchmark/data/pdbs/get-initial-pdbs.py
@@ -1,42 +1,88 @@
 """Script to get initial PDBs for benchmark targets."""
 from pathlib import Path
-
 import requests
+
 
 rcsb_url = "https://files.rcsb.org/view/"
 
-# Hen egg white lysozyme (HEWL)
-pdb_id = "1P7E"
-initial_pdb = "gb3-1P7E.pdb"
 
-if not Path(f"{pdb_id}.pdb").exists():
-    with open(initial_pdb, "w") as pdb_file:
+def get_rcsb_file(pdb_id, pdb_path, model = None):
+
+    if pdb_path.exists():
+        return
+
+    with open(pdb_path, "w") as pdb_file:
         # Get PDB text from RCSB website
         pdb_text = requests.get(f"{rcsb_url}{pdb_id}.pdb").text
 
-        # Extract ATOM and TER lines
-        for line in pdb_text.split("\n"):
-            if line[:4] in ["ATOM", "TER "]:
-                pdb_file.write(f"{line}\n")
+        if model is None:
 
+            # Extract ATOM and TER lines for alternate location A
+            for line in pdb_text.split("\n"):
+                if line[:4] == "ATOM" and line[16] in {" ", "A"}:
+
+                    # Replace deuterium with hydrogen
+                    if line[12:16].lstrip()[0] == "D":
+                        pdb_file.write(
+                            f"{line[:12]}{line[12:16].replace('D', 'H', 1)}"
+                            f"{line[16:77]}{line[77].replace('D', 'H')}{line[78:]}\n"
+                        )
+                    else:
+                        pdb_file.write(f"{line}\n")
+
+                elif line[:3] == "TER":
+                    pdb_file.write(f"{line}\n")
+
+        else:
+
+            # Extract specified model
+            print_lines = False
+            for line in pdb_text.split("\n"):
+                if line[:6] == "ENDMDL":
+                    print_lines = False
+
+                if print_lines:
+                    pdb_file.write(f"{line}\n")
+
+                if line[:14] == "MODEL        1":
+                    print_lines = True
+
+
+# Bovine pancreatic trypsin inhibitor (BPTI)
+pdb_id = "1PIT"
+nmr_model = 1
+initial_pdb_path = Path(f"bpti-{pdb_id}-model-{nmr_model}.pdb")
+get_rcsb_file(pdb_id, initial_pdb_path, model = nmr_model)
+
+pdb_id = "5PTI"
+initial_pdb_path = Path(f"bpti-{pdb_id}.pdb")
+get_rcsb_file(pdb_id, initial_pdb_path)
+
+# Streptococcal protein G third immunoglobulin-binding domain (GB3)
+pdb_id = "1P7E"
+initial_pdb_path = Path(f"gb3-{pdb_id}.pdb")
+get_rcsb_file(pdb_id, initial_pdb_path)
+
+pdb_id = "1IGD"
+initial_pdb_path = Path(f"gb3-{pdb_id}.pdb")
+get_rcsb_file(pdb_id, initial_pdb_path)
 
 # Hen egg white lysozyme (HEWL)
 pdb_id = "1E8L"
-initial_pdb = "hewl-1E8L-model-1.pdb"
+nmr_model = 1
+initial_pdb_path = Path(f"hewl-{pdb_id}-model-{nmr_model}.pdb")
+get_rcsb_file(pdb_id, initial_pdb_path, model = nmr_model)
 
-if not Path(f"{pdb_id}.pdb").exists():
-    with open(initial_pdb, "w") as pdb_file:
-        # Get PDB text from RCSB website
-        pdb_text = requests.get(f"{rcsb_url}{pdb_id}.pdb").text
+pdb_id = "193L"
+initial_pdb_path = Path(f"hewl-{pdb_id}.pdb")
+get_rcsb_file(pdb_id, initial_pdb_path)
 
-        # Extract first model
-        print_lines = False
-        for line in pdb_text.split("\n"):
-            if line[:6] == "ENDMDL":
-                print_lines = False
+# Human ubiquitin (Ubq)
+pdb_id = "1D3Z"
+nmr_model = 1
+initial_pdb_path = Path(f"ubq-{pdb_id}-model-{nmr_model}.pdb")
+get_rcsb_file(pdb_id, initial_pdb_path, model = nmr_model)
 
-            if print_lines:
-                pdb_file.write(f"{line}\n")
-
-            if line[:14] == "MODEL        1":
-                print_lines = True
+pdb_id = "1UBQ"
+initial_pdb_path = Path(f"ubq-{pdb_id}.pdb")
+get_rcsb_file(pdb_id, initial_pdb_path)
