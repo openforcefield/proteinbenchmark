be25d6038a5a3fde338712ec7e9ac7675f677797
Chapin Cavender <chapincavender@gmail.com>
2025-01-23 10:48:19 -0800

    GB3 NMR FFs after NMR fit bugfix

--- proteinbenchmark/force_fields.py
+++ proteinbenchmark/force_fields.py
@@ -466,6 +466,21 @@ force_fields = {
         "water_model": "opc3",
         "water_model_file": Path(ff_directory, "opc3-1.0.0.offxml"),
     },
+    "null-0.0.3-pair-nmr-1e4-2-opc3": {
+        "force_field_file": Path(ff_directory, "null-0.0.3-pair-nmr-1e4-2-opc3-NAGL.offxml"),
+        "water_model": "opc3",
+        "water_model_file": Path(ff_directory, "opc3-1.0.0.offxml"),
+    },
+    "null-0.0.3-pair-nmr-1e4-3-opc3": {
+        "force_field_file": Path(ff_directory, "null-0.0.3-pair-nmr-1e4-3-opc3-NAGL.offxml"),
+        "water_model": "opc3",
+        "water_model_file": Path(ff_directory, "opc3-1.0.0.offxml"),
+    },
+    "null-0.0.3-pair-nmr-1e4-cum-2-opc3": {
+        "force_field_file": Path(ff_directory, "null-0.0.3-pair-nmr-1e4-cum-2-opc3-NAGL.offxml"),
+        "water_model": "opc3",
+        "water_model_file": Path(ff_directory, "opc3-1.0.0.offxml"),
+    },
     "null-0.0.3-pair-nmr-1e5-2-opc3": {
         "force_field_file": Path(ff_directory, "null-0.0.3-pair-nmr-1e5-2-opc3-NAGL.offxml"),
         "water_model": "opc3",
