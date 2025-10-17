ca1e749d1c4b2bb7e681215348626e7dca0e47a1
Lily Wang <lily.wang@openforcefield.org>
2025-10-17 01:10:27 -0400

    add alpha tip3p

--- proteinbenchmark/force_fields.py
+++ proteinbenchmark/force_fields.py
@@ -226,6 +226,11 @@ force_fields = {
         "water_model": "opc3",
         "water_model_file": Path(ff_directory, "opc3-1.0.0.offxml"),
     },
+    "null-0.0.3-4-mer-aaqaa3-1e3-tip3p": {
+        "force_field_file": Path(ff_directory, "null-0.0.3-4-mer-aaqaa3-1e3-opc3-NAGL.offxml"),
+        "water_model": "tip3p",
+        "water_model_file": None,
+    },
     "null-0.0.3-4-mer-aaqaa3-1e3-2-opc3": {
         "force_field_file": Path(ff_directory, "null-0.0.3-4-mer-aaqaa3-1e3-2-opc3-NAGL.offxml"),
         "water_model": "opc3",
